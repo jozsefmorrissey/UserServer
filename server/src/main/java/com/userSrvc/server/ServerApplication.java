eabb8eb7d6fc435661f1e693bd00d36dc672afc5
jtmorri2@illinois.com <jtmorri2@illinois.edu>
2019-02-08 22:14:35 -0500

    fixed hard coded values

--- server/src/main/java/com/userSrvc/server/ServerApplication.java
+++ server/src/main/java/com/userSrvc/server/ServerApplication.java
@@ -32,27 +32,27 @@ public class ServerApplication {
 	public static void main(String[] args) throws IOException {
         SpringApplication application = new SpringApplication(ServerApplication.class);
         Properties properties = new Properties();
+		String dbUrl;// = "jdbc:oracle:thin:@" + GenUtils.getPassword("DB_URL", token, configPort);
+		dbUrl="jdbc:oracle:thin:@qfxvhm8okemwoc8_db201902042116_low?TNS_ADMIN=/home/ubuntu/Wallet/";
+		String dbpass;// = GenUtils.getPassword("dbPass", token, configPort);
+		dbpass = "iey0jee3souKaisheiJu5kaichishi";
+		String dbUser = "HLWA";//GenUtils.getPassword("DB_USER", token, configPort);
+		properties.put("spring.datasource.password", dbpass);
+		properties.put("spring.datasource.url", dbUrl);
+		properties.put("spring.datasource.username", dbUser);
 
-      if (args.length > 0) {
-				String token = args[0];
-				String configPort = args [1];
-				String dbpass = GenUtils.getPassword("dbPass", token, configPort);
-				dbpass = "iey0jee3souKaisheiJu5kaichishi";
-				String dbUrl = "jdbc:oracle:thin:@" + GenUtils.getPassword("DB_URL", token, configPort);
-				dbUrl="jdbc:oracle:thin:@qfxvhm8okemwoc8_db201902042116_low?TNS_ADMIN=/home/ubuntu/Wallet/";
-				String dbUser = GenUtils.getPassword("DB_USER", token, configPort);
-				String port = GenUtils.getPassword("PORT", token, configPort);
-				System.out.println(dbpass + ":" + dbUser + ":" + dbUrl + ":" + port);
-	//	      properties.put("spring.datasource.password", "ENC(" + dbPassEnc + ")");
-				properties.put("spring.datasource.password", dbpass);
-				properties.put("spring.datasource.url", dbUrl);
-				properties.put("spring.datasource.username", dbUser);
-				properties.put("server.port", port);
-		} else {
-			InputStream input = null;
-			input = new FileInputStream("./src/main/resources/application-test.properties");
-			properties.load(input);
-		}
+//      if (args.length > 0) {
+//				String token = args[0];
+//				String configPort = args [1];
+//				String port = GenUtils.getPassword("PORT", token, configPort);
+//				System.out.println(dbpass + ":" + dbUser + ":" + dbUrl + ":" + port);
+//	//	      properties.put("spring.datasource.password", "ENC(" + dbPassEnc + ")");
+//				properties.put("server.port", port);
+//		} else {
+//			InputStream input = null;
+//			input = new FileInputStream("./src/main/resources/application-test.properties");
+//			properties.load(input);
+//		}
 
         application.setDefaultProperties(properties);
         application.run(args);
