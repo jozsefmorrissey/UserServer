5bd316316c01b4ea1985926d5af7b848e581f17a
jtmorri2@illinois.com <jtmorri2@illinois.edu>
2019-02-15 18:15:56 -0500

    shorter service name

--- server/src/main/java/com/userSrvc/server/ServerApplication.java
+++ server/src/main/java/com/userSrvc/server/ServerApplication.java
@@ -1,6 +1,7 @@
 package com.userSrvc.server;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -12,10 +13,6 @@ import com.userSrvc.server.utils.GenUtils;
 
 @SpringBootApplication
 public class ServerApplication {
-
-	private static String dbpass;// = GenUtils.getPassword("dbPass", token, configPort);
-	static{dbpass = "iey0jee3souKaisheiJu5kaichishi";}
-
 
 //    @Bean("jasyptStringEncryptor")
 //    public StringEncryptor stringEncryptor() {
@@ -47,22 +44,61 @@ public class ServerApplication {
 //		}
 	}
 
-
-	public static void main(String[] args) throws IOException {
-        SpringApplication application = new SpringApplication(ServerApplication.class);
-        Properties properties = new Properties();
+	private static void cloudSetupHardCoded(String...args) {
+		SpringApplication application = new SpringApplication(ServerApplication.class);
+		Properties properties = new Properties();
 		String dbUrl;																														// = "jdbc:oracle:thin:@" + GenUtils.getPassword("DB_URL", token, configPort);
-		dbUrl="jdbc:oracle:thin:@qfxvhm8okemwoc8_db201902042116_low?TNS_ADMIN=/home/ubuntu/Wallet";
+		dbUrl="jdbc:oracle:thin:@db201902042116_low?TNS_ADMIN=/home/ubuntu/Wallet";
 		String dbUser = "HLWA";																												//GenUtils.getPassword("DB_USER", token, configPort);
+//		properties.put("spring.datasource.password", dbpass);
+		properties.put("spring.datasource.url", dbUrl);
+		properties.put("spring.datasource.username", dbUser);
+		String dbpass = "iey0jee3souKaisheiJu5kaichishi";
+
+		System.out.println("DB URL: '" + dbUrl + "'");
+		System.out.println("DB USER: '" + dbUser + "'");
+		System.out.println("DB PASS: 'youllJustHaveToTrustMe'");
+
+		application.setDefaultProperties(properties);
+		application.run(args);
+	}
+
+	private static void setup(String...args) throws IOException {
+		SpringApplication application = new SpringApplication(ServerApplication.class);
+		Properties properties = new Properties();
+
+	if (args.length > 0) {
+		String token = args[0];
+		String configPort = args [1];
+		String port = GenUtils.getPassword("PORT", token, configPort);
+		String dbUrl = "jdbc:oracle:thin:@" + GenUtils.getPassword("DB_URL", token, configPort);
+		String dbUser = GenUtils.getPassword("DB_USER", token, configPort);
+		String dbpass = GenUtils.getPassword("dbPass", token, configPort);
+
 		properties.put("spring.datasource.password", dbpass);
 		properties.put("spring.datasource.url", dbUrl);
 		properties.put("spring.datasource.username", dbUser);
 
 		System.out.println("DB URL: '" + dbUrl + "'");
 		System.out.println("DB USER: '" + dbUser + "'");
-		System.out.println("DB PASS: 'youllJustHaveToTrustMe'");
+		System.out.println("DB PASS: '" + dbpass + "'");
+		System.out.println(dbpass + ":" + dbUser + ":" + dbUrl + ":" + port);
+	//	      properties.put("spring.datasource.password", "ENC(" + dbPassEnc + ")");
+		properties.put("server.port", port);
+	} else {
+		InputStream input = null;
+		input = new FileInputStream("./src/main/resources/application-test.properties");
+		properties.load(input);
+	}
 
-        application.setDefaultProperties(properties);
-        application.run(args);
+		application.setDefaultProperties(properties);
+		application.run(args);
+
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		cloudSetupHardCoded(args);
+		// setup(args);
 	}
 }
