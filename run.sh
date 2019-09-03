fe0c47b4c9d8c8137872dd3da4fded28b0884da0
jtmorri2@illinois.com <jtmorri2@illinois.edu>
2019-09-03 00:12:00 -0400

    updated run.sh to run prod config

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 java -jar ./target/server-0.0.1-SNAPSHOT.jar
 
-cd ./universal-user-client && mvn install &
-cd ./server && mvn spring-boot:run
+cd ./universal-user-client &&
+mvn install &&
+cd ../server &&
+mvn -Dspring.profiles.active=prod spring-boot:run
